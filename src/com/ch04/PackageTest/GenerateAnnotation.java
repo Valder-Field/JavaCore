8d5b586df8087a96a2edb5ac4ccf31e91f547412
zhengwen <1016585170@qq.com>
2020-03-31 18:35:01 +0800

    update note ch04 and add note ch05

---
+++ src/com/ch04/PackageTest/GenerateAnnotation.java
@@ -0,0 +1,24 @@
+package com.ch04.PackageTest;
+
+/**
+ * test {@code}
+ * @author Administrator
+ *
+ */
+public class GenerateAnnotation {
+
+	/**
+	 * The "Hearts" card suit
+	 */
+	public static final int HEARTS = 1;
+	
+	/**
+	 * Raises the salary of an employee
+	 * @param byPercent the percentage by whick to raise the salary(e.g. 10 means 10%)
+	 * @return the amount of the raise
+	 */
+	public double raiseSalary(double byPercent) {
+		System.out.println("Hello World!");
+		return 66.66;
+	}
+}
---
+++ src/com/ch04/PackageTest/PackageTest.java
@@ -0,0 +1,14 @@
+package com.ch04.PackageTest;
+
+import static java.lang.System.*;
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// because of the static import statement, we don't hava to use System.out here
+		out.print("Hello World!");
+	}
+
+}
